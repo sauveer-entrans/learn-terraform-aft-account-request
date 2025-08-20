95709b9b480f7f3c5c3c914433214f7ab36e1ab9
Jenkins CI <jenkins@trivelta.com>
2025-08-20 20:32:24 +0000

    Update main.tf for account: Admin-App-Stg

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,26 @@ module "sandbox" {
 source = "./modules/aft-account-request"
 
 control_tower_parameters = {
-  AccountEmail               = "aws+adminapp@trivelta.com"
-  AccountName                = "Admin-App-Dev"
-  ManagedOrganizationalUnit = "Dev"
+  AccountEmail               = "aws+adminappstg@trivelta.com"
+  AccountName                = "Admin-App-Stg"
+  ManagedOrganizationalUnit = "Stg"
   SSOUserEmail               = "aws@trivelta.com"
   SSOUserFirstName           = "Admin"
-  SSOUserLastName            = "App"
+  SSOUserLastName            = "AppSTG"
 }
 
 account_tags = {
-  env     = "dev"
+  env     = "stg"
   project = "Trivelta"
 }
 
 change_management_parameters = {
   change_requested_by = "aws+jenkinsNonProd@trivelta.com"
-  change_reason       = "Create a new Dev AWS Account"
+  change_reason       = "Create a new Stg AWS Account"
 }
 
 custom_fields = {
-  group = "dev"
+  group = "stg"
 }
 
 account_customizations_name = "sandbox"
