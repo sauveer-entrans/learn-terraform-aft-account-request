2aa3c22c3cb6b46b1cb317be0119f9f60bf85133
sauveer-entrans <sauveer.entrans.io>
2025-09-03 23:50:56 +0000

    etst account 02

---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "account_name" { type = string }
+variable "organizational_unit_name" { type = string }
+variable "account_email" { type = string }
+variable "sso_user_email" { type = string }
+variable "sso_user_first_name" { type = string }
+variable "sso_user_last_name" { type = string }
