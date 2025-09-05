a6d0f67826440a1dfd5978c194bfdfaa2d31b5fe
sauveer-entrans <sauveer.entrans.io>
2025-09-05 06:02:37 +0000

    removed variables.tf

--- terraform/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "account_name" { type = string }
-variable "organizational_unit_name" { type = string }
-variable "account_email" { type = string }
-variable "sso_user_email" { type = string }
-variable "sso_user_first_name" { type = string }
-variable "sso_user_last_name" { type = string }
