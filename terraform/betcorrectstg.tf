86ed2c509029c0f5d789b63ead53022e8027adba
Jenkins CI <jenkins@trivelta.com>
2025-09-22 08:22:30 +0000

    Added tf for account: betcorrectstg

---
+++ terraform/betcorrectstg.tf
@@ -0,0 +1,28 @@
+module "betcorrectstg" {
+source = "./modules/aft-account-request"
+
+control_tower_parameters = {
+  AccountEmail               = "aws+ajaybetcorrectstg@trivelta.com"
+  AccountName                = "betcorrectstg"
+  ManagedOrganizationalUnit = "Stg"
+  SSOUserEmail               = "aws@trivelta.com"
+  SSOUserFirstName           = "Ajay"
+  SSOUserLastName            = "Ramnani"
+}
+
+account_tags = {
+  env     = "stg"
+  project = "Trivelta"
+}
+
+change_management_parameters = {
+  change_requested_by = "aws+jenkinsNonProd@trivelta.com"
+  change_reason       = "Create a new Dev AWS Account"
+}
+
+custom_fields = {
+  group = "stg"
+}
+
+account_customizations_name = "sandbox"
+}
