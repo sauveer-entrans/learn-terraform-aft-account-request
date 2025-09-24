924bdd518a05fced8803a64460ff236a203c8623
Jenkins CI <jenkins@trivelta.com>
2025-09-24 12:40:31 +0000

    Added tf for account: luckbox-prd

---
+++ terraform/luckbox-prd.tf
@@ -0,0 +1,28 @@
+module "luckbox-prd" {
+source = "./modules/aft-account-request"
+
+control_tower_parameters = {
+  AccountEmail               = "aws+luckbox@trivelta.com"
+  AccountName                = "luckbox-prd"
+  ManagedOrganizationalUnit = "Prd"
+  SSOUserEmail               = "aws@trivelta.com"
+  SSOUserFirstName           = "Tyler"
+  SSOUserLastName            = "Graeber"
+}
+
+account_tags = {
+  env     = "prod"
+  project = "Trivelta"
+}
+
+change_management_parameters = {
+  change_requested_by = "aws+jenkinsNonProd@trivelta.com"
+  change_reason       = "Promote staging to production"
+}
+
+custom_fields = {
+  group = "prod"
+}
+
+account_customizations_name = "sandbox"
+}
