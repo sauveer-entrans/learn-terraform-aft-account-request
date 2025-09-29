69921cccf308f307987ac4886bd98e5a1d0d47f2
Jenkins CI <jenkins@trivelta.com>
2025-09-29 05:09:05 +0000

    Added tf for account: betcorrect10-prd

---
+++ terraform/betcorrect10-prd.tf
@@ -0,0 +1,28 @@
+module "betcorrect10-prd" {
+source = "./modules/aft-account-request"
+
+control_tower_parameters = {
+  AccountEmail               = "aws+betcorrect10@trivelta.com"
+  AccountName                = "betcorrect10-prd"
+  ManagedOrganizationalUnit = "Prd"
+  SSOUserEmail               = "aws@trivelta.com"
+  SSOUserFirstName           = "Ajay"
+  SSOUserLastName            = "Ramnani"
+}
+
+account_tags = {
+  env     = "prod"
+  project = "Trivelta"
+}
+
+change_management_parameters = {
+  change_requested_by = "aws+jenkinsNonProd@trivelta.com"
+  change_reason       = "Promote staging to production"
+}
+
+custom_fields = {
+  group = "prod"
+}
+
+account_customizations_name = "sandbox"
+}
