aefd29a7bcf263e511039d55dceba8f8f7b850cf
Jenkins CI <jenkins@trivelta.com>
2025-11-03 15:35:57 +0000

    Added tf for account:

---
+++ terraform/.tf
@@ -0,0 +1,28 @@
+module "" {
+source = "./modules/aft-account-request"
+
+control_tower_parameters = {
+  AccountEmail               = ""
+  AccountName                = ""
+  ManagedOrganizationalUnit = ""
+  SSOUserEmail               = ""
+  SSOUserFirstName           = ""
+  SSOUserLastName            = ""
+}
+
+account_tags = {
+  env     = ""
+  project = ""
+}
+
+change_management_parameters = {
+  change_requested_by = ""
+  change_reason       = ""
+}
+
+custom_fields = {
+  group = ""
+}
+
+account_customizations_name = ""
+}
