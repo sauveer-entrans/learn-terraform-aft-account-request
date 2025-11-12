5f44e0d126a6b8914ef43bdc1e4c67c694519bb6
Jenkins CI <jenkins@trivelta.com>
2025-11-12 09:13:50 +0000

    Added tf for account: test03-dev

---
+++ terraform/test03-dev.tf
@@ -0,0 +1,28 @@
+module "test03-dev" {
+source = "./modules/aft-account-request"
+
+control_tower_parameters = {
+  AccountEmail               = "aws+test03dev@trivelta.com"
+  AccountName                = "test03-dev"
+  ManagedOrganizationalUnit = "Dev"
+  SSOUserEmail               = "aws@trivelta.com"
+  SSOUserFirstName           = "Chris"
+  SSOUserLastName            = "McMahon"
+}
+
+account_tags = {
+  env     = "dev"
+  project = "Trivelta"
+}
+
+change_management_parameters = {
+  change_requested_by = "aws+jenkinsNonProd@trivelta.com"
+  change_reason       = "Create a new Dev AWS Account"
+}
+
+custom_fields = {
+  group = "dev"
+}
+
+account_customizations_name = "sandbox"
+}
