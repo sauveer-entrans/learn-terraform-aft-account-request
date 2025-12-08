1c3e2b10fd394fc210ec54731c33d75daa48b9ed
Jenkins CI <jenkins@trivelta.com>
2025-12-08 20:42:59 +0000

    Added tf for account: infinito-stg

---
+++ terraform/infinito-stg.tf
@@ -0,0 +1,28 @@
+module "infinito-stg" {
+source = "./modules/aft-account-request"
+
+control_tower_parameters = {
+  AccountEmail               = "aws+infinitostg@trivelta.com"
+  AccountName                = "infinito-stg"
+  ManagedOrganizationalUnit = "Stg"
+  SSOUserEmail               = "aws@trivelta.com"
+  SSOUserFirstName           = "Andres"
+  SSOUserLastName            = "Gonzalez"
+}
+
+account_tags = {
+  env     = "stg"
+  project = "Trivelta"
+}
+
+change_management_parameters = {
+  change_requested_by = "aws+jenkinsNonProd@trivelta.com"
+  change_reason       = "Create a new Dev AWS Account"
+}
+
+custom_fields = {
+  group = "stg"
+}
+
+account_customizations_name = "sandbox"
+}
