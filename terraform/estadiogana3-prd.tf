8658fad783c34a1d7ccec5af2e6e27cdd65a0eb3
Jenkins CI <jenkins@trivelta.com>
2026-03-05 10:47:49 +0000

    Added tf for account: estadiogana3-prd

---
+++ terraform/estadiogana3-prd.tf
@@ -0,0 +1,28 @@
+module "estadiogana3-prd" {
+source = "./modules/aft-account-request"
+
+control_tower_parameters = {
+  AccountEmail               = "aws+estadiogana3-prd@trivelta.com"
+  AccountName                = "estadiogana3-prd"
+  ManagedOrganizationalUnit = "Prd"
+  SSOUserEmail               = "aws@trivelta.com"
+  SSOUserFirstName           = "Chris"
+  SSOUserLastName            = "McMahon"
+}
+
+account_tags = {
+  env     = "prod"
+  project = "Trivelta"
+}
+
+change_management_parameters = {
+  change_requested_by = "aws+jenkinsNonProd@trivelta.com"
+  change_reason       = "Create a new AWS Account"
+}
+
+custom_fields = {
+  group = "prod"
+}
+
+account_customizations_name = "sandbox"
+}
